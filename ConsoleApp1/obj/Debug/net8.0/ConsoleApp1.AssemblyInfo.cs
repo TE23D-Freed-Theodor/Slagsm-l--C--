818f60f04f27cff36cba1019c3f26e1192ed235c
Theodor Freed <theodor.freed@elev.ga.ntig.se>
2024-09-27 11:19:02 +0200

    test

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a2df74c8196c9bee824f52624055e9c8f988e13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ecde682d1d074ce1fd1a286f5e9b9f045c1bb62")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
